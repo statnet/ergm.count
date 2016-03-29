d13c2710006d273b4aa3a053fba912e43b9c3d7c
Pavel N. Krivitsky <pavel@uow.edu.au>
2016-03-29 06:52:12 +0000

    Added/updated copyright headers.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm.count/trunk@14382 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/InitWtMHP.R
+++ R/InitWtMHP.R
@@ -1,3 +1,12 @@
+#  File R/InitWtMHP.R in package ergm.count, part of the Statnet suite
+#  of packages for network analysis, http://statnet.org .
+#
+#  This software is distributed under the GPL-3 license.  It is free,
+#  open source, and has the attribution requirements (GPL Section 7) at
+#  http://statnet.org/attribution
+#
+#  Copyright 2008-2016 Statnet Commons
+#######################################################################
 InitWtMHP.Poisson <- function(arguments, nw, response) {
   MHproposal <- list(name = "Poisson", inputs=NULL)
   MHproposal
