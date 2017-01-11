6f06dfeeedef705571717b454d6e7d1be86acb67
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-01-11 04:21:46 +0000

    Updated count and rank change stats to call ZERO_ALL_CHANGESTATS() explicitly.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm.count/trunk@14488 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- src/wtchangestats.c
+++ src/wtchangestats.c
@@ -26,6 +26,7 @@
  Turns a Poisson-reference or geometric-reference ERGM into a Conway-Maxwell-Poisson distribution
 *****************/
 WtD_CHANGESTAT_FN(d_sumlogfactorial){
+  ZERO_ALL_CHANGESTATS();
   EXEC_THROUGH_TOGGLES({
       CHANGE_STAT[0] += lgamma1p(NEWWT)-lgamma1p(OLDWT);
   });
