ef8e9ee3531ded10ebfd704263a49efd7eb9c78e
Pavel N. Krivitsky <pavel@uow.edu.au>
2018-08-25 19:59:55 +1000

    Added and/or updated copyright headers.

--- src/wtchangestats.h
+++ src/wtchangestats.h
@@ -5,16 +5,7 @@
  *  open source, and has the attribution requirements (GPL Section 7) at
  *  http://statnet.org/attribution
  *
- *  Copyright 2008-2016 Statnet Commons
- */
-/*  File src/wtchangestats.h in package ergm.count, part of the Statnet suite
- *  of packages for network analysis, http://statnet.org .
- *
- *  This software is distributed under the GPL-3 license.  It is free,
- *  open source, and has the attribution requirements (GPL Section 7) at
- *  http://statnet.org/attribution
- *
- *  Copyright 2008-2016 Statnet Commons
+ *  Copyright 2008-2018 Statnet Commons
  */
 #ifndef WTCHANGESTATS_H
 #define WTCHANGESTATS_H
