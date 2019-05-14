c128c4151fd969d794206238bf3dff8b35ae8f7b
Pavel N. Krivitsky <pavel@uow.edu.au>
2019-05-15 05:58:59 +1000

    Updated copyright year and headers.

--- R/InitErgmReference.R
+++ R/InitErgmReference.R
@@ -1,11 +1,11 @@
 #  File R/InitErgmReference.R in package ergm.count, part of the Statnet suite
-#  of packages for network analysis, http://statnet.org .
+#  of packages for network analysis, https://statnet.org .
 #
 #  This software is distributed under the GPL-3 license.  It is free,
 #  open source, and has the attribution requirements (GPL Section 7) at
-#  http://statnet.org/attribution
+#  https://statnet.org/attribution
 #
-#  Copyright 2008-2018 Statnet Commons
+#  Copyright 2008-2019 Statnet Commons
 #######################################################################
 InitErgmReference.Poisson <- function(lhs.nw, ...){
   list(name="Poisson", init_methods = c("CD","zeros"))
