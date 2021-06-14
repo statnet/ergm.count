23ed4eb570e6c86e470dbce21bbef20795d9160b
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2021-06-14 20:35:03 +1000

    Updated copyright year and headers.

--- R/InitErgmReference.R
+++ R/InitErgmReference.R
@@ -1,12 +1,12 @@
-#  File R/InitErgmReference.R in package ergm.count, part of the Statnet suite
-#  of packages for network analysis, https://statnet.org .
+#  File R/InitErgmReference.R in package ergm.count, part of the
+#  Statnet suite of packages for network analysis, https://statnet.org .
 #
 #  This software is distributed under the GPL-3 license.  It is free,
 #  open source, and has the attribution requirements (GPL Section 7) at
-#  https://statnet.org/attribution
+#  https://statnet.org/attribution .
 #
-#  Copyright 2008-2019 Statnet Commons
-#######################################################################
+#  Copyright 2008-2021 Statnet Commons
+################################################################################
 InitErgmReference.Poisson <- function(lhs.nw, ...){
   list(name="Poisson", init_methods = c("CD","zeros"))
 }
--- R/data.R
+++ R/data.R
@@ -1,3 +1,12 @@
+#  File R/data.R in package ergm.count, part of the
+#  Statnet suite of packages for network analysis, https://statnet.org .
+#
+#  This software is distributed under the GPL-3 license.  It is free,
+#  open source, and has the attribution requirements (GPL Section 7) at
+#  https://statnet.org/attribution .
+#
+#  Copyright 2008-2021 Statnet Commons
+################################################################################
 #' Karate club social network of Zachary (1977)
 #'
 #'
