a659cd7946a114002a4a61332825a6827f5892b6
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2022-05-23 22:00:54 +1000

    Removed a test that depended on ergm 4.2.2+.

--- tests/testthat/test-valued-sim.R
+++ tests/testthat/test-valued-sim.R
@@ -13,40 +13,40 @@ testnet3u <- network.initialize(3, directed=FALSE) # 3 dyads
 # Poisson-reference
 test_that("Poisson-reference ERGM with mean 2, TNT proposal", {
   set.seed(0)
-  expect_message(
-    s <- simulate(testnet3d~sum, nsim=1000, reference=~Poisson, response="w", coef=log(2), output="stats", control=control.simulate.formula(MCMC.prop.args=list(p0=0.8)), verbose=TRUE),
-    "\\bergm.count:MH_DiscTNT\\b"
-  )
+  ## expect_message(
+    s <- simulate(testnet3d~sum, nsim=1000, reference=~Poisson, response="w", coef=log(2), output="stats", control=control.simulate.formula(MCMC.prop.args=list(p0=0.8)), verbose=TRUE)## ,
+  ##   "\\bergm.count:MH_DiscTNT\\b"
+  ## )
   test <- approx.hotelling.diff.test(s/6, mu0=2)
   expect_gt(test$p.value, 0.001)
 })
 
 test_that("Poisson-reference ERGM with mean 2 and constraints, TNT proposal", {
   set.seed(0)
-  expect_message(
-    s <- simulate(testnet3d~sum, nsim=1000, reference=~Poisson, response="w", constraints=~egocentric(I(c(FALSE,FALSE,TRUE))), coef=log(2), output="stats", control=control.simulate.formula(MCMC.prop.args=list(p0=0.5)), verbose=TRUE),
-    "\\bergm.count:MH_DiscTNT\\b"
-  )
+  ## expect_message(
+    s <- simulate(testnet3d~sum, nsim=1000, reference=~Poisson, response="w", constraints=~egocentric(I(c(FALSE,FALSE,TRUE))), coef=log(2), output="stats", control=control.simulate.formula(MCMC.prop.args=list(p0=0.5)), verbose=TRUE)## ,
+  ##   "\\bergm.count:MH_DiscTNT\\b"
+  ## )
   test <- approx.hotelling.diff.test(s/2, mu0=2)
   expect_gt(test$p.value, 0.001)
 })
 
 test_that("Poisson-reference ERGM with mean 2, random proposal", {
   set.seed(0)
-  expect_message(
-    s <- simulate(testnet3d~sum, nsim=1000, reference=~Poisson, response="w", coef=log(2), output="stats", control=control.simulate.formula(MCMC.prop.weights="random"), verbose=TRUE),
-    "\\bergm.count:MH_Disc\\b"
-  )
+  ## expect_message(
+    s <- simulate(testnet3d~sum, nsim=1000, reference=~Poisson, response="w", coef=log(2), output="stats", control=control.simulate.formula(MCMC.prop.weights="random"), verbose=TRUE)## ,
+  ##   "\\bergm.count:MH_Disc\\b"
+  ## )
   test <- approx.hotelling.diff.test(s/6, mu0=2)
   expect_gt(test$p.value, 0.001)
 })
 
 test_that("Poisson-reference ERGM with mean 2, zero-modified proposal", {
   set.seed(0)
-  expect_message(
-    s <- simulate(testnet3d~sum, nsim=1000, reference=~Poisson, response="w", coef=log(2), output="stats", control=control.simulate.formula(MCMC.prop.weights="0inflated", MCMC.prop.args=list(p0=0.8)), verbose=TRUE),
-    "\\bergm.count:MH_ZIPoisson\\b"
-  )
+  ## expect_message(
+    s <- simulate(testnet3d~sum, nsim=1000, reference=~Poisson, response="w", coef=log(2), output="stats", control=control.simulate.formula(MCMC.prop.weights="0inflated", MCMC.prop.args=list(p0=0.8)), verbose=TRUE)## ,
+  ##   "\\bergm.count:MH_ZIPoisson\\b"
+  ## )
   test <- approx.hotelling.diff.test(s/6, mu0=2)
   expect_gt(test$p.value, 0.001)
 })
@@ -54,10 +54,10 @@ test_that("Poisson-reference ERGM with mean 2, zero-modified proposal", {
 ## Geometric-reference
 test_that("Geometric-reference ERGM with mean 2, TNT proposal", {
   set.seed(0)
-  expect_message(
-    s <- simulate(testnet3d~sum, nsim=1000, reference=~Geometric, response="w", coef=log(2/3), output="stats", verbose=TRUE),
-    "\\bergm.count:MH_DiscTNT\\b"
-  )
+  ## expect_message(
+    s <- simulate(testnet3d~sum, nsim=1000, reference=~Geometric, response="w", coef=log(2/3), output="stats", verbose=TRUE)## ,
+  ##   "\\bergm.count:MH_DiscTNT\\b"
+  ## )
   test <- approx.hotelling.diff.test(s/6, mu0=2)
   expect_gt(test$p.value, 0.001)
 })
@@ -65,10 +65,10 @@ test_that("Geometric-reference ERGM with mean 2, TNT proposal", {
 ## Binomial-reference
 test_that("Binomial-reference ERGM with mean 5 trials and probability of success 0.4 for a mean 2, TNT proposal", {
   set.seed(0)
-  expect_message(
-    s <- simulate(testnet3d~sum, nsim=1000, reference=~Binomial(5), response="w", coef=log(.4/.6), output="stats", verbose=TRUE),
-    "\\bergm.count:MH_DiscTNT\\b"
-  )
+  ## expect_message(
+    s <- simulate(testnet3d~sum, nsim=1000, reference=~Binomial(5), response="w", coef=log(.4/.6), output="stats", verbose=TRUE)## ,
+  ##   "\\bergm.count:MH_DiscTNT\\b"
+  ## )
   test <- approx.hotelling.diff.test(s/6, mu0=2)
   expect_gt(test$p.value, 0.001)
 })
@@ -76,10 +76,10 @@ test_that("Binomial-reference ERGM with mean 5 trials and probability of success
 ## DiscUnif-reference
 test_that("DiscUnif-reference ERGM with range [0,...,4] for a mean 2, TNT proposal", {
   set.seed(0)
-  expect_message(
-    s <- simulate(testnet3u~sum, nsim=1000, reference=~DiscUnif(0,4), response="w", coef=0, output="stats", verbose=TRUE),
-    "\\bergm.count:MH_DiscTNT\\b"
-  )
+  ## expect_message(
+    s <- simulate(testnet3u~sum, nsim=1000, reference=~DiscUnif(0,4), response="w", coef=0, output="stats", verbose=TRUE)## ,
+  ##   "\\bergm.count:MH_DiscTNT\\b"
+  ## )
   expect_equal(min(s)/3, 0)
   expect_equal(max(s)/3, 4)
   test <- approx.hotelling.diff.test(s/3, mu0=2)
@@ -88,10 +88,10 @@ test_that("DiscUnif-reference ERGM with range [0,...,4] for a mean 2, TNT propos
 
 test_that("DiscUnif-reference ERGM with range [1,...,3] for a mean 2, random proposal should be autodetected", {
   set.seed(0)
-  expect_message(
-    s <- simulate(testnet3u~sum, nsim=1000, reference=~DiscUnif(1,3), response="w", coef=0, output="stats", verbose=TRUE),
-    "\\bergm.count:MH_Disc\\b"
-  )
+  ## expect_message(
+    s <- simulate(testnet3u~sum, nsim=1000, reference=~DiscUnif(1,3), response="w", coef=0, output="stats", verbose=TRUE)## ,
+  ##   "\\bergm.count:MH_Disc\\b"
+  ## )
   expect_equal(min(s)/3, 1)
   expect_equal(max(s)/3, 3)
   test <- approx.hotelling.diff.test(s/3, mu0=2)
@@ -100,10 +100,10 @@ test_that("DiscUnif-reference ERGM with range [1,...,3] for a mean 2, random pro
 
 test_that("DiscUnif-reference ERGM with range [-1,...,2] for a mean 1/2, TNT proposal", {
   set.seed(0)
-  expect_message(
-    s <- simulate(testnet3u~sum, nsim=1000, reference=~DiscUnif(-1,2), response="w", coef=0, output="stats", verbose=TRUE),
-    "\\bergm.count:MH_DiscTNT\\b"
-  )
+  ## expect_message(
+    s <- simulate(testnet3u~sum, nsim=1000, reference=~DiscUnif(-1,2), response="w", coef=0, output="stats", verbose=TRUE)## ,
+  ##   "\\bergm.count:MH_DiscTNT\\b"
+  ## )
   expect_equal(min(s)/3, -1)
   expect_equal(max(s)/3, 2)
   test <- approx.hotelling.diff.test(s/3, mu0=1/2)
@@ -118,10 +118,10 @@ EDU <- function(theta, a, b){
 
 test_that("DiscUnif-reference ERGM with range [-1,...,1] and a truncated geometric, TNT proposal", {
   set.seed(0)
-  expect_message(
-    s <- simulate(testnet3u~sum, nsim=1000, reference=~DiscUnif(-1,1), response="w", coef=1, output="stats", verbose=TRUE),
-    "\\bergm.count:MH_DiscTNT\\b"
-  )
+  ## expect_message(
+    s <- simulate(testnet3u~sum, nsim=1000, reference=~DiscUnif(-1,1), response="w", coef=1, output="stats", verbose=TRUE)## ,
+  ##   "\\bergm.count:MH_DiscTNT\\b"
+  ## )
   expect_equal(min(s)/3, -1)
   expect_equal(max(s)/3, 1)
   test <- approx.hotelling.diff.test(s/3, mu0=EDU(1, -1, 1))
@@ -130,10 +130,10 @@ test_that("DiscUnif-reference ERGM with range [-1,...,1] and a truncated geometr
 
 test_that("DiscUnif-reference ERGM with range [0,...,2] and a truncated geometric, TNT proposal", {
   set.seed(0)
-  expect_message(
-    s <- simulate(testnet3u~sum, nsim=1000, reference=~DiscUnif(0,2), response="w", coef=1, output="stats", verbose=TRUE),
-    "\\bergm.count:MH_DiscTNT\\b"
-  )
+  ## expect_message(
+    s <- simulate(testnet3u~sum, nsim=1000, reference=~DiscUnif(0,2), response="w", coef=1, output="stats", verbose=TRUE)## ,
+  ##   "\\bergm.count:MH_DiscTNT\\b"
+  ## )
   expect_equal(min(s)/3, 0)
   expect_equal(max(s)/3, 2)
   test <- approx.hotelling.diff.test(s/3, mu0=EDU(1, 0, 2))
