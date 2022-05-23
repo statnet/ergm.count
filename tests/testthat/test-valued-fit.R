5aea64e751ca40f2bc916aaf78013b7df894cdea
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2022-05-23 17:52:04 +1000

    Updated LICENSE year and copyright headers.

--- tests/testthat/test-valued-fit.R
+++ tests/testthat/test-valued-fit.R
@@ -1,3 +1,12 @@
+#  File tests/testthat/test-valued-fit.R in package ergm.count, part of the
+#  Statnet suite of packages for network analysis, https://statnet.org .
+#
+#  This software is distributed under the GPL-3 license.  It is free,
+#  open source, and has the attribution requirements (GPL Section 7) at
+#  https://statnet.org/attribution .
+#
+#  Copyright 2008-2022 Statnet Commons
+################################################################################
 test_that("Poisson-reference ERGM fit", {
   set.seed(0)
 
--- tests/testthat/test-valued-sim.R
+++ tests/testthat/test-valued-sim.R
@@ -1,3 +1,12 @@
+#  File tests/testthat/test-valued-sim.R in package ergm.count, part of the
+#  Statnet suite of packages for network analysis, https://statnet.org .
+#
+#  This software is distributed under the GPL-3 license.  It is free,
+#  open source, and has the attribution requirements (GPL Section 7) at
+#  https://statnet.org/attribution .
+#
+#  Copyright 2008-2022 Statnet Commons
+################################################################################
 testnet3d <- network.initialize(3, directed=TRUE) # 6 dyads
 testnet3u <- network.initialize(3, directed=FALSE) # 3 dyads
 
